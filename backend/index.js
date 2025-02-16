eac82c6ed6de65b861cb4108661ae224165769ca
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-02-16 10:07:20 +0100

    connect db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Listening Port
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+//app.use("/api/user", userRoute);
