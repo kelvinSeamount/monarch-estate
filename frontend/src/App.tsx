253fe76a22de18988b8c2ee7da503e30960f72b1
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-02-16 23:12:49 +0100

    add route pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
-  return <div className="text-red-400 font-family font-bold">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
