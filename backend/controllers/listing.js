bf37ccd1ee2e0022d9107c2d2433f185a40ef819
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-04-11 23:44:33 +0200

    add delete listing api

--- backend/controllers/listing.js
+++ backend/controllers/listing.js
@@ -1,8 +1,28 @@
 import Listing from "../models/listing.models.js";
+import { errorHandler } from "../utils/error.js";
+
 export const createListing = async (req, resizeBy, next) => {
   try {
     const listing = await Listing.create(req.body);
     return resizeBy.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  //check if listing exits
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  // listing exits & check if user matches Id
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Listing deleted successfully");
   } catch (error) {
     next(error);
   }
--- backend/route/listing.js
+++ backend/route/listing.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createListing } from "../controllers/listing.js";
+import { createListing, deleteListing } from "../controllers/listing.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
+router.delete("/delete", verifyUser, deleteListing);
 export default router;
